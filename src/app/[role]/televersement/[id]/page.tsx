342a4a0867725ea4a466982e69eb09a3da58a3a7
jessicakossibale <189719444+jessicakossibale@users.noreply.github.com>
2024-12-13 09:24:51 +0100

    update packages and fix build (#19)

--- src/app/[role]/televersement/[id]/page.tsx
+++ src/app/[role]/televersement/[id]/page.tsx
@@ -1,14 +1,16 @@
-export default async function Devis({
-  params,
+import { use } from 'react';
+
+export default function Devis({
+  params: initialParams,
 }: {
-  params: { role: string; id: string };
+  params: Promise<{ id: string; role: string }>;
 }) {
-  const { role, id } = await params;
+  const params = use(initialParams);
 
   return (
     <div>
-      <h1>Devis ID: {id}</h1>
-      <h3>Role: {role}</h3>
+      <h1>Devis ID: {params.id}</h1>
+      <h3>Role: {params.role}</h3>
     </div>
   );
 }
