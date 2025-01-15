ec979c7641ecedc647e9c5fc09ec9d9b340d9494
Jessica Kossibale <189719444+jessicakossibale@users.noreply.github.com>
2025-01-15 11:07:40 +0100

    correct accessibility

--- src/app/accessibilite/page.tsx
+++ src/app/accessibilite/page.tsx
@@ -6,12 +6,12 @@ export default function Accessibilite() {
     <section className='fr-container-fluid fr-py-10w'>
       <div className='fr-container'>
         <h1 className='fr-mb-6w text-[var(--text-title-grey)]'>
-          {wording.accessibilty.title}
+          {wording.accessibility.title}
         </h1>
-        <p>{richTextParser(wording.accessibilty.first_paragraph)}</p>
-        <p>{richTextParser(wording.accessibilty.second_paragraph)}</p>
-        <p>{wording.accessibilty.third_paragraph}</p>
-        <p>{richTextParser(wording.accessibilty.fourth_paragraph)}</p>
+        <p>{richTextParser(wording.accessibility.first_paragraph)}</p>
+        <p>{richTextParser(wording.accessibility.second_paragraph)}</p>
+        <p>{wording.accessibility.third_paragraph}</p>
+        <p>{richTextParser(wording.accessibility.fourth_paragraph)}</p>
       </div>
     </section>
   );
