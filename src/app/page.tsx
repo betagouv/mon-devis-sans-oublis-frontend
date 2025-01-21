7750689b3afbd2198d870312969b57617135e158
jessicakossibale <189719444+jessicakossibale@users.noreply.github.com>
2025-01-21 06:27:39 +0100

    fix landing page wording (#56)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ import {
   Tile,
 } from '@/components';
 import { useIsDesktop } from '@/hooks';
+import { richTextParser } from '@/utils';
 import wording from '@/wording';
 
 export default function Home() {
@@ -24,28 +25,26 @@ export default function Home() {
         <div className='fr-container'>
           <div className='flex flex-col md:flex-row md:justify-between'>
             <div className='fr-col-12 fr-col-md-5'>
-              <h1>{wording.homepage.section_examples.title}</h1>
+              <h1>{wording.homepage.hero_section.title}</h1>
               <ul className='fr-raw-list fr-badges-group fr-mb-3w flex flex-wrap gap-4'>
-                {wording.homepage.section_examples.badges.map(
-                  (badge, index) => (
-                    <li key={index}>
-                      <Badge
-                        label={badge.label}
-                        variant={BadgeVariant.BLUE_LIGHT}
-                      />
-                    </li>
-                  )
-                )}
+                {wording.homepage.hero_section.badges.map((badge, index) => (
+                  <li key={index}>
+                    <Badge
+                      label={badge.label}
+                      variant={BadgeVariant.BLUE_LIGHT}
+                    />
+                  </li>
+                ))}
               </ul>
               <Link {...wording.homepage.check_quote_button} />
             </div>
             <div className='mt-10 md:mt-0 flex justify-center'>
               <Image
-                alt='Mon Devis Sans Oublis'
+                alt={wording.homepage.hero_section.image.alt}
                 className='w-auto h-[220px] md:h-[300px] lg:h-[376px] object-contain'
                 height={376}
                 sizes='100vw'
-                src='/images/homepage.png'
+                src={wording.homepage.hero_section.image.src}
                 width={0}
               />
             </div>
@@ -54,37 +53,27 @@ export default function Home() {
       </section>
       <section className='fr-container-fluid fr-py-10w'>
         <div className='fr-container'>
-          <h2>A quoi sert Mon Devis Sans Oublis ?</h2>
+          <h2>{wording.homepage.explanation_cards.title_1}</h2>
           <div className='fr-grid-row fr-grid-row--gutters flex flex-col md:flex-row'>
-            <div className='fr-col-12 fr-col-md-4 flex-1'>
-              <CardImage
-                description='Identifiez les mentions manquantes aux attendus réglementaires des aides.'
-                image='/images/quote_control.png'
-                title='Pré-contrôler en un clic les devis de rénovation énergétique'
-              />
-            </div>
-            <div className='fr-col-12 fr-col-md-4 flex-1'>
-              <CardImage
-                description='Accélérez l’instruction des aides de rénovation énergétique en évitant les allers-retours avec les instructeurs grâce à des devis conformes.'
-                image='/images/get_help.png'
-                title='Obtenez vos aides de rénovation plus rapidement'
-              />
-            </div>
-            <div className='fr-col-12 fr-col-md-4 flex-1'>
-              <CardImage
-                description='Conception d’une interface simple qui accompagne les usagers dans la lecture et la correction des devis.'
-                image='/images/change_quote.png'
-                title='Améliorez la satisfaction des usagers'
-              />
-            </div>
+            {wording.homepage.explanation_cards.image_cards.map(
+              (card, index) => (
+                <div className='fr-col-12 fr-col-md-4 flex-1' key={index}>
+                  <CardImage
+                    description={card.description}
+                    image={card.image}
+                    title={card.title}
+                  />
+                </div>
+              )
+            )}
           </div>
           <div className='fr-grid-row fr-grid-row--center'>
             <h2 className='fr-mt-8w'>
-              {wording.homepage.section_examples.subtitle}
+              {wording.homepage.explanation_cards.title_2}
             </h2>
             <div className='lg:block overflow-x-auto overflow-y-hidden whitespace-nowrap w-full scrollbar-none touch-pan-x [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden'>
               <ul className='fr-raw-list fr-grid-row flex-nowrap gap-6 !justify-start w-max lg:flex-wrap lg:!justify-center lg:w-full'>
-                {wording.homepage.section_examples.cards.map((card, index) => (
+                {wording.homepage.explanation_cards.cards.map((card, index) => (
                   <li
                     className='flex-none w-[147px] whitespace-normal flex flex-col items-center'
                     key={index}
@@ -104,60 +93,31 @@ export default function Home() {
             </div>
           </div>
           <h2 className='fr-mt-6w fr-mb-5w'>
-            {wording.homepage.section_steps.title}
+            {wording.homepage.explanation_cards.title_3}
           </h2>
           <div className='flex flex-col md:flex-row gap-10'>
             <Image
-              alt='Mon Devis Sans Oublis'
+              alt={wording.homepage.explanation_cards.image.alt}
               className='w-auto lg:h-[420px] md:h-[380px] h-[224px] object-contain'
               height={420}
               sizes='100vw'
-              src='/images/three_steps.png'
+              src={wording.homepage.explanation_cards.image.src}
               width={0}
             />
             <div className='flex flex-col'>
-              <BlockNumber
-                number={1}
-                title={
-                  <span>
-                    <span className='font-bold mr-1.5'>
-                      Déposez votre devis
-                    </span>
-                    afin qu&apos;il soit analysé par notre outil qui vérifie que
-                    tous les attendus réglementaires soient bien présents.
-                  </span>
-                }
-              />
-              <BlockNumber
-                className='my-2'
-                number={2}
-                title={
-                  <span>
-                    <span className='font-bold mr-1.5'>
-                      Découvrez instantanément les suggestions de corrections
-                    </span>
-                    adaptées aux demandes d&apos;aides de rénovation
-                    énergétique.
-                  </span>
-                }
-              />
-              <BlockNumber
-                number={3}
-                title={
-                  <span>
-                    Informez les parties prenantes (par ex: artisans,
-                    mandataires) et
-                    <span className='font-bold ml-1.5'>
-                      partagez vos corrections personnalisées.
-                    </span>
-                  </span>
-                }
-              />
+              {wording.homepage.explanation_cards.number_blocks.map(
+                (block, index) => (
+                  <BlockNumber
+                    className='fr-mb-2w'
+                    key={index}
+                    number={block.number}
+                    title={richTextParser(block.title)}
+                  />
+                )
+              )}
               <p className='bg-[var(--background-alt-blue-france)] p-4'>
                 <span className='fr-icon-restart-line mr-1 ml-0 text-[var(--text-title-blue-france)]' />
-                {
-                  "Une fois corrigé, soumettez à nouveau votre devis jusqu'à ce qu'il soit conforme."
-                }
+                {wording.homepage.explanation_cards.correction}
               </p>
               <div className='flex items-end md:mt-auto mt-6 justify-center md:justify-start'>
                 <Link {...wording.homepage.check_quote_button} />
@@ -171,62 +131,36 @@ export default function Home() {
           <div className='fr-grid-row fr-grid-row--gutters flex flex-wrap'>
             <div className='fr-col-12 fr-col-md-6 flex'>
               <Card
-                image='/images/ministere_transition_ecologique.png'
-                title='Qui sommes-nous ?'
+                image={wording.homepage.about_us.who_are_we.image}
+                title={wording.homepage.about_us.who_are_we.title}
               >
                 <p className='text-[var(--text-mention-grey)]'>
-                  Mon Devis Sans Oublis est un service public gratuit qui
-                  vérifie en quelques secondes la conformité des devis aux
-                  attendus réglementaires des aides à la rénovation énergétique.
-                  <br />
-                  <br />
-                  {
-                    "Nous sommes un service public lancé par le Ministère de la Transition Ecologique et la Direction du Numérique (DINUM) sous la forme d'une start-up d'Etat. Elle est en phase d'expérimentation avant un déploiement massif. N'hésitez pas à nous faire part de vos retours et suggestions d'améliorations."
-                  }
+                  {richTextParser(
+                    wording.homepage.about_us.who_are_we.description
+                  )}
                 </p>
               </Card>
             </div>
             <div className='fr-col-12 fr-col-md-6 flex'>
-              <Card title='Quelles sont nos missions ?'>
+              <Card title={wording.homepage.about_us.our_missions.title}>
                 <div className='[&_p]:m-0'>
-                  <div className='flex flex-row gap-2 mt-8'>
-                    <span className='fr-icon-map-pin-2-line text-[var(--background-action-high-blue-france)] mt-1' />
-                    <div className='flex flex-col'>
-                      <p className='font-bold text-[var(--text-title-grey)]'>
-                        Fluidifier l’instructions des aides
-                      </p>
-                      <p className='text-[var(--text-mention-grey)]'>
-                        Création d’un modèle d’analyse automatique qui compare
-                        les devis de rénovation énergétique avec les exigences
-                        des demandes d’aides.
-                      </p>
-                    </div>
-                  </div>
-                  <div className='flex flex-row gap-2 mt-8'>
-                    <span className='fr-icon-timer-line text-[var(--background-action-high-blue-france)] mt-1' />
-                    <div className='flex flex-col'>
-                      <p className='font-bold text-[var(--text-title-grey)]'>
-                        Réduire les délais d’instruction d’aide
-                      </p>
-                      <p className='text-[var(--text-mention-grey)]'>
-                        Réduction du nombre d’allers-retours avec les
-                        instructeurs d’aides avec un pré-contrôle gratuit et
-                        instantané.
-                      </p>
-                    </div>
-                  </div>
-                  <div className='flex flex-row gap-2 mt-8'>
-                    <span className='fr-icon-heart-line text-[var(--background-action-high-blue-france)] mt-1' />
-                    <div className='flex flex-col'>
-                      <p className='font-bold text-[var(--text-title-grey)]'>
-                        Améliorer la satisfaction des usagers
-                      </p>
-                      <p className='text-[var(--text-mention-grey)]'>
-                        Conception d’une interface simple qui accompagne les
-                        usagers dans la lecture et la correction des devis.
-                      </p>
-                    </div>
-                  </div>
+                  {wording.homepage.about_us.our_missions.missions.map(
+                    (mission, index) => (
+                      <div className='flex flex-row gap-2 mt-8' key={index}>
+                        <span
+                          className={`${mission.icon} text-[var(--background-action-high-blue-france)] mt-1`}
+                        />
+                        <div className='flex flex-col'>
+                          <p className='font-bold text-[var(--text-title-grey)]'>
+                            {mission.title}
+                          </p>
+                          <p className='text-[var(--text-mention-grey)]'>
+                            {mission.description}
+                          </p>
+                        </div>
+                      </div>
+                    )
+                  )}
                 </div>
               </Card>
             </div>
@@ -245,17 +179,20 @@ export default function Home() {
                 className='fr-col-12 fr-col-md-6 fr-col-lg-3 flex'
                 key={index}
               >
-                <Tile
-                  className={
-                    isDesktop
-                      ? 'hidden md:flex w-full'
-                      : 'block md:hidden w-full'
-                  }
-                  description={card.description}
-                  href={card.url}
-                  image={isDesktop ? card.image : undefined}
-                  title={card.title}
-                />
+                {isDesktop ? (
+                  <Tile
+                    description={card.description}
+                    image={card.image}
+                    href={card.url}
+                    title={card.title}
+                  />
+                ) : (
+                  <Tile
+                    description={card.description}
+                    href={card.url}
+                    title={card.title}
+                  />
+                )}
               </li>
             ))}
           </ul>
