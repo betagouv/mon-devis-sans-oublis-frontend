d00c353120d35774b96e9b05c475273967a09667
jessicakossibale <189719444+jessicakossibale@users.noreply.github.com>
2025-01-22 16:12:52 +0100

    add statistics page (#62)

---
+++ src/types/statistics.ts
@@ -0,0 +1,7 @@
+export interface Statistics {
+  average_quote_check_cost: number;
+  average_quote_check_errors_count: number;
+  average_quote_check_processing_time: number;
+  quote_checks_count: number;
+  unique_visitors_count: number;
+}
