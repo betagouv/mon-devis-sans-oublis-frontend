b90722e7b0812c498f3a47a8cc06afd9f5f50860
jessicakossibale <189719444+jessicakossibale@users.noreply.github.com>
2025-01-22 10:40:30 +0100

    remove contact button from header (#59)

--- src/page-sections/home/WhoAreYou.tsx
+++ src/page-sections/home/WhoAreYou.tsx
@@ -1,12 +1,7 @@
-'use client';
-
 import { Tile } from '@/components';
-import { useIsDesktop } from '@/hooks';
 import wording from '@/wording';
 
 export default function WhoAreYou() {
-  const isDesktop = useIsDesktop();
-
   return (
     <section className='fr-container-fluid fr-py-10w'>
       <div className='fr-container'>
@@ -17,20 +12,12 @@ export default function WhoAreYou() {
         <ul className='fr-raw-list fr-grid-row fr-grid-row--gutters fr-grid-row--center items-stretch'>
           {wording.homepage.section_who_are_you.cards.map((card, index) => (
             <li className='fr-col-12 fr-col-md-6 fr-col-lg-3 flex' key={index}>
-              {isDesktop ? (
-                <Tile
-                  description={card.description}
-                  image={card.image}
-                  href={card.url}
-                  title={card.title}
-                />
-              ) : (
-                <Tile
-                  description={card.description}
-                  href={card.url}
-                  title={card.title}
-                />
-              )}
+              <Tile
+                description={card.description}
+                image={card.image}
+                href={card.url}
+                title={card.title}
+              />
             </li>
           ))}
         </ul>
