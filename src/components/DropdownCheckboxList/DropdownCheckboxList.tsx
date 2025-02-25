1d3c21e47640f5b111ae8e87d0e08dc3ba222808
jessicakossibale <189719444+jessicakossibale@users.noreply.github.com>
2025-02-25 14:53:21 +0100

    update packages (#111)

--- src/components/DropdownCheckboxList/DropdownCheckboxList.tsx
+++ src/components/DropdownCheckboxList/DropdownCheckboxList.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, JSX } from 'react';
 
 import CheckboxGroup from '../CheckboxGroup/CheckboxGroup';
 
